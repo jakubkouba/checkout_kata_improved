2dedf27df73f12d3928d2d14b2083c8ec6584e86
Jakub Adler <adler.jakub@gmail.com>
2020-01-28 16:27:18 +0000

    Price is 80 when basket contains product A and B: Passing

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,6 +1,6 @@
 class Checkout
 
-  attr_reader :items
+  attr_reader :items, :price_rules
 
   def initialize(price_rules)
     @price_rules = price_rules
@@ -13,6 +13,11 @@ class Checkout
 
   def total
     return 0 if items.empty?
-    50
+
+    items.reduce(0) do |total, item|
+      item_price_rules = price_rules[item]
+      total += item_price_rules[:unit_price]
+      total
+    end
   end
 end
